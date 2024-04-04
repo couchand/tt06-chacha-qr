4aabe4b31d69c0fcb9d6eb0c1d8f8cb4e65522de
Andrew Dona-Couch <hi@andrewcou.ch>
2024-04-04 14:11:50 -0400

    more ambitious test

--- test/test.py
+++ test/test.py
@@ -31,23 +31,33 @@ async def test_project(dut):
   dut.uio_in.value = 0x00
   await ClockCycles(dut.clk, 1)
 
-  #dut.ui_in.value = 30
-  #dut.uio_in.value = 0x11
-  #await ClockCycles(dut.clk, 1)
+  dut._log.info("Set 1 to 30")
+  dut.ui_in.value = 30
+  await ClockCycles(dut.clk, 1)
+  dut.uio_in.value = 0x11
+  await ClockCycles(dut.clk, 2)
+  dut.uio_in.value = 0x01
+  await ClockCycles(dut.clk, 1)
 
-  #dut.ui_in.value = 40
-  #dut.uio_in.value = 0x12
-  #await ClockCycles(dut.clk, 1)
+  dut._log.info("Set 2 to 40")
+  dut.ui_in.value = 40
+  await ClockCycles(dut.clk, 1)
+  dut.uio_in.value = 0x12
+  await ClockCycles(dut.clk, 2)
+  dut.uio_in.value = 0x02
+  await ClockCycles(dut.clk, 1)
 
   dut._log.info("Check that 0 is 20")
   dut.uio_in.value = 0x00
   await ClockCycles(dut.clk, 1)
   assert dut.uo_out.value == 20
 
-  #dut.uio_in.value = 0x01
-  #await ClockCycles(dut.clk, 1)
-  #assert dut.uo_out.value == 30
+  dut._log.info("Check that 1 is 30")
+  dut.uio_in.value = 0x01
+  await ClockCycles(dut.clk, 1)
+  assert dut.uo_out.value == 30
 
-  #dut.uio_in.value = 0x02
-  #await ClockCycles(dut.clk, 1)
-  #assert dut.uo_out.value == 40
+  dut._log.info("Check that 2 is 40")
+  dut.uio_in.value = 0x02
+  await ClockCycles(dut.clk, 1)
+  assert dut.uo_out.value == 40
